e4d811d0c5886c4c9ec7a86f34fefe61997471e9
Daniel Inman <deinman@users.noreply.github.com>
2019-03-03 11:25:46 -0500

    Grains

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > 64)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        ulong ret = 1;
+
+        if (n == 1)
+        {
+            return ret;
+        }
+
+        for (var i=1; i<n; i++)
+        {
+            ret = ret * 2;
+        }
+
+        return ret;
+    }
+
+    public static ulong Total()
+    {
+        ulong ret = 0;
+
+        for (int i = 1; i <= 64; i++)
+        {
+            ret += Square(i);
+        }
+
+        return ret;
+    }
+}
