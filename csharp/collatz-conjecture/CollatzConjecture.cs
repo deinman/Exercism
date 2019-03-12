8bcd726af33a2086ba9d50421c9349c0ba9b2376
Daniel Inman <deinman@users.noreply.github.com>
2019-03-11 20:48:02 -0400

    CollatzConjecture
    Simple implementation of the Collatz conjecture.

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        var counter = 0;
+        var n = number;
+
+        while (n != 1)
+        {
+            counter++;
+            n = Collatz(n);
+        }
+
+        return counter;
+    }
+
+    private static int Collatz(int n)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+
+        if (n % 2 == 0)
+        {
+            return n / 2;
+        }
+        else
+        {
+            return (3 * n) + 1;
+        }
+    }
+}
